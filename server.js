14310c478739afeb08d3f53b4dcd43ef16d92c4e
Jaden Andrews <59934265+jadenandrews831@users.noreply.github.com>
2023-04-23 17:57:53 -0400

    update login post

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const express = require("express");
-const bodyParser = require('body-parser')
-var users = require('./users.js') 
+const bodyParser = require('body-parser');
+var users = require('./users.js'); 
 
 const app = express();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-db = users.
+const db = new users.Users('users.db');
+
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname+'/src/registration.html')
+  res.sendFile(__dirname+'/src/registration.html');
 });
 
 app.get("/registration.html", (req, res) => {
-  res.sendFile(__dirname+'/src/registration.html')
+  res.sendFile(__dirname+'/src/registration.html');
 });
 
-app.get("/style.css", (req, res) => {
-  res.sendFile(__dirname+'/src/style.css')
-})
-
 app.post("/registration.html", (req, res) => {
-  console.log()
-  res.sendFile(__dirname+"/src/index.html")
+  console.log(req.body['First Name']);
+  db.addtoTables(req.body);
+  res.sendFile(__dirname+"/src/index.html");
 })
 
 app.get("/login.html", (req, res) => {
-  res.sendFile(__dirname+'/src/login.html')
-  console.log(req.body)
+  res.sendFile(__dirname+'/src/login.html');
+  console.log(req.body);
 });
+
+app.post("/login.html", (req, res) => {
+  console.log(req.body['username'])
+})
+
+app.get("/style.css", (req, res) => {
+  res.sendFile(__dirname+'/src/style.css');
+})
 
 app.listen(3000, () => {
   console.log("Listening on Port http://localhost:3000");
